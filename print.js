13a074ccd24a13818ac009225bd8a64133ad5bb2
Sivin Saji <sivinsaji22@gmail.com>
2021-04-19 20:17:41 +0530

    Sum file added & print file updated

--- print.js
+++ print.js
@@ -1,3 +1,6 @@
 var name = "Cross Roads"
 var n =10
 console.log('hello world'+" "+name +" "+n) 
+var name="Crossroads"
+var name1=new String("Crossroads")
+console.log(name.length)
---
+++ sum.js
@@ -0,0 +1,4 @@
+var num1=10
+var num2=20
+var num3=num1+num2
+console.log("Sum of 2 Numbers: "+num3)
