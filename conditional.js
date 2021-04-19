5df9da2009c26518ce61f192ec4f487bfdfd684b
Sivin Saji <sivinsaji22@gmail.com>
2021-04-19 21:11:06 +0530

    condition added

---
+++ conditional.js
@@ -0,0 +1,15 @@
+var read=require('readline-sync')
+var num1=read.question("Enter the 1st Number :")
+var num2=read.question("Eneter the 2nd Number: ")
+if(num1>num2)
+{
+    console.log(num1+" "+"is largest number" )
+}
+else if(num1==num2)
+{
+    console.log("Entered number is same")
+}
+else
+{
+    console.log(num2+" "+"is largest number" )
+}
---
+++ inputing.js
@@ -0,0 +1,3 @@
+var read=require('readline-sync')
+var value=read.question("Enter a number ")
+console.log("Your Entered Number is: "+value)
