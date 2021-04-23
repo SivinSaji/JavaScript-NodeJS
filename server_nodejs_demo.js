5cad36e702a8f45ca9be065e5907181dafadb85b
Sivin Saji <sivinsaji22@gmail.com>
2021-04-23 15:14:51 +0530

    serever-basics: html as response to a user for his request,hosting a html page in nodeJS server

--- server_nodejs_demo.js
+++ server_nodejs_demo.js
@@ -1,7 +1,7 @@
 //HERE WE HAVE 2 SERVER USING NodeJS
 //This is  how we create a server using NodeJS.Here we create 2 servers we can create the rever using this 2 methods the commonly used method is the first method.
-var http=require('http')                   //Here we call the librarry 
-http.createServer(function(req,res){       //But this code will run when we host server(when we run code in terminal)
+var http=require('http')                   //Here we call the librarry http 
+http.createServer(function(req,res){       //createServer is a inbuilt module of http library.But this code will run when we host server(when we run code in terminal)
     res.write('Its a response from a server')   //But this function will only run when a request come to the server(ie. when a user search the web site in the browser). Here we write Crossroads it will be displaye in the browser
     res.end()                                   //It is used to understand the server the response is finished
 }).listen(5000)                                   //Here 5000 is the port number.listen(5000) This is used to search the site using localhost:5000 in the same device we host the server using the code we only get this site at that device only.
